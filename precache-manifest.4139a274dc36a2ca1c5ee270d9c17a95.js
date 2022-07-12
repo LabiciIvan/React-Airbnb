0bde7f2c1195f70a1d556304d14385c6de3433d5
Ioan Labici <ioan.labici@amdaris.com>
2022-07-12 08:29:48 +0300

    Updates

---
+++ precache-manifest.4139a274dc36a2ca1c5ee270d9c17a95.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "087fa98782e6833932a0",
+    "url": "/React-Airbnb/static/css/main.d951a090.chunk.css"
+  },
+  {
+    "revision": "087fa98782e6833932a0",
+    "url": "/React-Airbnb/static/js/main.087fa987.chunk.js"
+  },
+  {
+    "revision": "97c00e585b9fe313b141",
+    "url": "/React-Airbnb/static/js/1.97c00e58.chunk.js"
+  },
+  {
+    "revision": "a7579b779aa675eb2864",
+    "url": "/React-Airbnb/static/js/runtime~main.a7579b77.js"
+  },
+  {
+    "revision": "df0e9555f5e2324dc3dc8f9004081baa",
+    "url": "/React-Airbnb/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "%7Busername%7D.github.io/precache-manifest.7d60bb16b257b54f74a9cfc7e32b77dc.js"
+  "/React-Airbnb/precache-manifest.4139a274dc36a2ca1c5ee270d9c17a95.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("%7Busername%7D.github.io/index.html", {
+workbox.routing.registerNavigationRoute("/React-Airbnb/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
